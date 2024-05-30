cbe63783a9f6e1836bc815230867e0c03395f7dd
Marina Reis <marinareis.mr@gmail.com>
2024-05-30 17:46:20 -0300

    testes unitarios

---
+++ src/test/java/com/marina/bibilioteca/BibiliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marina.bibilioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibiliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
